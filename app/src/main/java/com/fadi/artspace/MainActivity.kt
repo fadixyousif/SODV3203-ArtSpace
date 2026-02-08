67c5aba241555d2a5511c776617a63ade874d8ee
Fadi Y <fadiyoux@gmail.com>
2026-02-07 23:20:15 -0700

    Feat: Include Image Box and Add Test Image.

--- app/src/main/java/com/fadi/artspace/MainActivity.kt
+++ app/src/main/java/com/fadi/artspace/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -21,7 +23,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
@@ -44,12 +49,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateImage() {
-    Box(
-        contentAlignment = Alignment.Center,
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
-            .background(Color(0xFFecebf4))
+            .shadow(12.dp, RoundedCornerShape(8.dp))
+            .border(1.dp, Color.Blue, RoundedCornerShape(8.dp))
+            .background(Color(0xFFfefbff))
+            .padding(50.dp),
     ) {
-
+        Image(
+            painter = painterResource(R.drawable.Improvisation_31), // Improvisation 31 (Sea Battle) By Wassily Kandinsky (1913)
+            contentDescription = null
+        )
     }
 }
 
@@ -120,11 +132,14 @@ fun GenerateArtSpace() {
         verticalArrangement = Arrangement.Bottom,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
+            .background(Color(0xFF))
             //.padding(bottom = 16.dp)
             .fillMaxSize()
 
     ) {
-        CreateImage();
+        Spacer(modifier = Modifier.weight(1f))
+        CreateImage()
+        Spacer(modifier = Modifier.weight(1f))
         CreateDescription()
         CreateButtons()
     }
